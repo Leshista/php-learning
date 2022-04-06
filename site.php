7d42b5f6887e7ebb9b97918450cd6f755455ac1a
leshista <a.leshista@gmail.com>
2022-04-06 15:01:10 +0300

    Get to know basic data types in php

--- site.php
+++ site.php
@@ -5,12 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php learning dummy site</title>
 </head>
 
 <body>
     <?php
-    echo ('Hell o World');
+    // Basic Datatypes
+    $string = "To be or not to be";
+    $int = 1;
+    $float = 1.310230123120;
+    $boolean = true;
+    $null = null;
     ?>
 </body>
 
